982f9be3c30338bc7761e21c0463251490e4c079
Jacob Ruleaux <jakeruleaux@gmail.com>
2021-11-18 09:59:55 -0700

    install axios depends for getting twitter api

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -3,8 +3,12 @@ import { FETCH_TWEETS } from "./types";
 // url
 // https://api.twitter.com/1.1/search/tweets.json
 
+// GET https://api.twitter.com/1.1/collections/list.json?screen_name=twittermusic&count=1
+
+
+
 export const fetchTweets =() => async dispatch => {
-    const response = await ();
+    const response = await ('https://api.twitter.com/1.1/search/tweets.json');
 
     dispatch({ type: FETCH_TWEETS, payload: response.data });
 };
